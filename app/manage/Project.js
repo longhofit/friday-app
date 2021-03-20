fe0077f24ab99a1010904c443f51b018110e0fb6
Ho Le <lelongho998@gmail.com>
2021-03-20 20:42:46 +0700

    - Update project screen

--- app/manage/Project.js
+++ app/manage/Project.js
@@ -23,6 +23,7 @@ import {
 import { pxPhone } from '../../core/utils/utils';
 import ProjectService from '../services/project.service';
 import Entypo from 'react-native-vector-icons/Entypo'
+import AntDesign from 'react-native-vector-icons/AntDesign'
 
 export default ProjectsScreen = (props) => {
   const [projects, setProjects] = useState([]);
@@ -38,80 +39,106 @@ export default ProjectsScreen = (props) => {
   };
 
   return (
-    <ScrollView style={styles.container}>
-      <StatusBar barStyle="dark-content" />
-      {projects.map((item, index) => {
-        return (
-          <View
-            key={index}
-            style={styles.viewProject}>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-              <View style={{ width: '75%' }}>
-                <Text style={styles.name}>
-                  {item.name}
-                </Text>
+    <View style={{ flex: 1 }}>
+      <ScrollView style={styles.container}>
+        <StatusBar barStyle="dark-content" />
+        {projects.map((item, index) => {
+          let color;
+          switch (item.status) {
+            case 'NEW':
+              color = '#5584FF';
+              break;
+            case 'ARCHIVED':
+              color = '#D86667';
+              break;
+            case 'SUSPEND':
+              color = '#7D61C8';
+              break;
+            case 'RUNNING':
+              color = 'green';
+              break;
+            default:
+              break;
+          }
+          return (
+            <View
+              key={index}
+              style={[styles.viewProject,(index===projects.length-1)&&{marginBottom:pxPhone(80)}]}>
+              <View style={[styles.vertical, { backgroundColor: color }]}>
               </View>
-              <Entypo
-                name={'dots-three-vertical'}
-                size={pxPhone(15)}
-                color={'gray'}
-              />
-            </View>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingVertical: pxPhone(11) }}>
-              <View style={{ flex: 1 }}>
-                <View>
-                  <Text style={styles.label}>
-                    {'Status'}
+              <View style={{ flex: 1, padding: pxPhone(12) }}>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                  <View style={{ width: '75%' }}>
+                    <Text style={styles.name}>
+                      {item.name}
+                    </Text>
+                  </View>
+                  <Entypo
+                    name={'dots-three-vertical'}
+                    size={pxPhone(15)}
+                    color={'gray'}
+                  />
+                </View>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingVertical: pxPhone(11) }}>
+                  <View style={{ flex: 1 }}>
+                    <View>
+                      <Text style={styles.label}>
+                        {'Status'}
+                      </Text>
+                      <Text style={[styles.value, { color }]}>
+                        {item.status}
+                      </Text>
+                    </View>
+                  </View>
+                  <View style={{ flex: 1, alignItems: 'flex-start' }}>
+                    <View>
+                      <Text style={styles.label}>
+                        {'Type'}
+                      </Text>
+                      <Text style={[styles.value]}>
+                        {item.type}
+                      </Text>
+                    </View>
+                  </View>
+                  <View style={{ flex: 1, alignItems: 'center' }}>
+                    <View>
+                      <Text style={styles.label}>
+                        {'Code'}
+                      </Text>
+                      <Text style={[styles.value, { fontWeight: 'bold' }]}>
+                        {item.code}
+                      </Text>
+                    </View>
+                  </View>
+                  <View style={{ flex: 1, alignItems: 'flex-end' }}>
+                    <Text style={styles.label}>
+                      {'Prefix'}
+                    </Text>
+                    <Text style={[styles.value, { fontWeight: 'bold' }]}>
+                      {item.ticketPrefix}
+                    </Text>
+                  </View>
+                </View>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                  <Text>
+                    {`${item.owner}`}
                   </Text>
-                  <Text style={[styles.value, { color: item.status === 'NEW' ? 'red' : 'orange' }]}>
-                    {item.status}
+                  <Text>
+                    {item.timeLogFrequency}
                   </Text>
                 </View>
               </View>
-              <View style={{ flex: 1, alignItems: 'flex-start' }}>
-                <View>
-                  <Text style={styles.label}>
-                    {'Type'}
-                  </Text>
-                  <Text style={[styles.value,{color:'blue'}]}>
-                    {item.type}
-                  </Text>
-                </View>
-              </View>
-              <View style={{ flex: 1, alignItems: 'center' }}>
-                <View>
-                  <Text style={styles.label}>
-                    {'Code'}
-                  </Text>
-                  <Text style={[styles.value,{fontWeight:'bold'}]}>
-                    {item.code}
-                  </Text>
-                </View>
-              </View>
-              <View style={{ flex: 1, alignItems: 'flex-end' }}>
-                <Text style={styles.label}>
-                  {'Prefix'}
-                </Text>
-                <Text style={[styles.value,{fontWeight:'bold'}]}>
-                  {item.ticketPrefix}
-                </Text>
-              </View>
             </View>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-              <Text>
-                <Text style={{ color: 'gray', }}>
-                  {'Owner: '}
-                </Text>
-                {`${item.owner}`}
-              </Text>
-              <Text>
-                {item.timeLogFrequency}
-              </Text>
-            </View>
-          </View>
-        );
-      })}
-    </ScrollView>
+          );
+        })}
+      </ScrollView>
+      <AntDesign
+        style={styles.icon}
+        name={'pluscircle'}
+        size={pxPhone(50)}
+        color={'#3753C7'}
+      />
+    </View>
   );
 };
 
@@ -125,13 +152,12 @@ const styles = StyleSheet.create({
     fontSize: pxPhone(14),
   },
   name: {
-    fontSize: pxPhone(18),
+    fontSize: pxPhone(17),
     fontWeight: 'bold',
   },
   viewProject: {
-    padding: pxPhone(15),
     margin: pxPhone(10),
-    // flexDirection: 'row',
+    flexDirection: 'row',
     width: '90%',
     backgroundColor: 'white',
     borderRadius: pxPhone(6),
@@ -150,5 +176,16 @@ const styles = StyleSheet.create({
   },
   value: {
     marginTop: pxPhone(3),
-  }
+  },
+  vertical: {
+    width: pxPhone(5),
+    height: '100%',
+    borderTopLeftRadius: pxPhone(6),
+    borderBottomStartRadius: pxPhone(6),
+  },
+  icon: {
+    position: 'absolute',
+    bottom:pxPhone(20),
+    right:pxPhone(20),
+  },
 });
